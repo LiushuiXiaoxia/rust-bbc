67140b52601b8bd4c49f38eb6d714fc2a10f8f5a
xiaqiulei <xiaqiulei@bilibili.com>
2025-06-15 16:14:40 +0800

    middleware

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
-mod routes;
 mod config;
+mod middleware;
+mod routes;
 
-use actix_web::{web, App, HttpServer, middleware::Logger};
-use env_logger::Env;
-use routes::index::{hello, health_check};
+use actix_web::{middleware::Logger, App, HttpServer};
 use config::load_config;
+use env_logger::Env;
+use middleware::RequestHandler;
+use routes::index::{health_check, hello};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-    
+
     let config = load_config();
     let addr = format!("http://{}:{}", config.server.host, config.server.port);
     println!("Server starting at {}", addr);
-    
+
     HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
+            .wrap(RequestHandler::new())
             .service(hello)
             .service(health_check)
     })
