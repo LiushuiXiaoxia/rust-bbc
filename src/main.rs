918aa05b0c31fd544ba926baee0d570382535d36
xiaqiulei <xiaqiulei@bilibili.com>
2025-06-15 16:01:31 +0800

    log

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 mod routes;
 mod config;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpServer, middleware::Logger};
+use env_logger::Env;
 use routes::index::{hello, health_check};
 use config::load_config;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    
     let config = load_config();
     let addr = format!("http://{}:{}", config.server.host, config.server.port);
     println!("Server starting at {}", addr);
     
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
             .service(hello)
             .service(health_check)
     })
