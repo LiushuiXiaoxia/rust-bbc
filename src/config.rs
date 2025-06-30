b95e2dc2d1b2c49ac78861c1777a174d24ed3d01
xiaqiulei <xiaqiulei@bilibili.com>
2025-06-30 17:31:43 +0800

    update

--- src/config.rs
+++ src/config.rs
@@ -1,21 +1,32 @@
-use serde::Deserialize;
-use std::fs;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ServerConfig {
     pub host: String,
     pub port: u16,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct S3Config {
+    pub region: String,
+    pub endpoint: String,
+    pub accessKey: String,
+    pub secretKey: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
     pub server: ServerConfig,
+    pub s3: S3Config,
 }
 
 pub fn load_config() -> Config {
-    let config_str = fs::read_to_string("Config.toml")
-        .expect("无法读取配置文件");
-    
-    toml::from_str(&config_str)
-        .expect("无法解析配置文件")
+    let dev = "Config.toml";
+    let prod = "Config.prod.toml";
+    let mut f = dev;
+    if rovkit::iokit::path_exists(prod) {
+        f = prod;
+    }
+    let s = rovkit::iokit::read_file_to_string(f).unwrap();
+    toml::from_str(&s).expect("无法解析配置文件")
 }
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,18 @@ mod util;
 use actix_web::{middleware::Logger, web, App, HttpServer};
 use config::load_config;
 use env_logger::Env;
+use log::info;
 use routes::cache::cache_handler;
 use routes::index::{health_check, hello};
+use rovkit::jsonkit;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     let config = load_config();
+    info!("config: {}", jsonkit::to_pretty_json(&config)?);
+
     let addr = format!("http://{}:{}", config.server.host, config.server.port);
     println!("Server starting at {}", addr);
 
